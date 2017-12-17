ef8ff5ddd641490d83c40bef2aa474963d4b8eee
Josh Waihi <josh.waihi@acquia.com>
2017-12-18 10:49:57 +1300

    Added Audit to assert no results should be found

---
+++ src/Audit/NoLogsFound.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drutiny\SumoLogic\Audit;
+
+use Drutiny\Sandbox\Sandbox;
+
+class NoLogsFound extends ApiEnabledAudit {
+
+  /**
+   * Find no records.
+   */
+  public function audit(Sandbox $sandbox)
+  {
+    list($sitegroup, $env) = explode('.', str_replace('@', '', $sandbox->drush()->getAlias()), 2);
+    $tokens['@sitegroup'] = $sitegroup;
+    $tokens['@environment'] = $env;
+
+    $query = $sandbox->getParameter('query');
+
+    $query = strtr($query, $tokens);
+
+    $records = $this->search($sandbox, $query);
+
+    return count($records) === 0;
+  }
+}
+
+ ?>
