1c1366e86bb430121fc08dff7b30b8be734a7912
Josh Waihi <josh.waihi@acquia.com>
2023-06-06 13:09:15 +1200

    Improve error reporting for metric Analysis.

--- src/Audit/MetricsAnalysis.php
+++ src/Audit/MetricsAnalysis.php
@@ -4,7 +4,9 @@ namespace Drutiny\SumoLogic\Audit;
 use Drutiny\Attribute\Parameter;
 use Drutiny\Attribute\Type;
 use Drutiny\Audit\AbstractAnalysis;
+use Drutiny\Audit\Exception\AuditException;
 use Drutiny\SumoLogic\Client;
+use GuzzleHttp\Exception\ClientException;
 
 /**
  * Analyse a metrics query response from SumoLogic.
@@ -34,10 +36,23 @@ class MetricsAnalysis extends AbstractAnalysis
             ]
         ];
 
-        $this->set('metrics', $api->getMetricsQueries(
-            queries: array_map(fn($q) => $this->processQuery($q), $this->getParameter('queries')),
-            timeRange: $timeRange
-        ));
+        try {
+            $this->set('metrics', $api->getMetricsQueries(
+                queries: array_map(fn($q) => $this->processQuery($q), $this->getParameter('queries')),
+                timeRange: $timeRange
+            ));
+        }
+        catch (ClientException $e) {
+            $payload = json_decode($e->getResponse()->getBody());
+            $messages = ['Sumologic API Error: ' . $payload->id];
+            foreach ($payload->errors as $error) {
+                $messages[] = $error->code;
+                $messages[] = $error->message;
+                $messages[] = '';
+            }
+            throw new AuditException(implode(PHP_EOL, $messages), 0, $e);
+        }
+       
     }
 
     protected function processQuery(array $query) {
