4767b5f0f4ac5963097469fe670318a289d07c2c
Josh Waihi <josh.waihi@acquia.com>
2023-06-15 14:14:56 +1200

    Add event subscriber to send logs to Sumologic if configured.

---
+++ src/EventSubscriber.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drutiny\SumoLogic;
+
+use Drutiny\AuditResponse\AuditResponse;
+use Drutiny\Report\Report;
+use Drutiny\Settings;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class EventSubscriber implements EventSubscriberInterface {
+
+    /**
+     * @var Array[]
+     */
+    private array $logs;
+
+    public function __construct(protected Settings $settings)
+    {
+        
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'report.create' => 'logReportCreate',
+        ];
+    }
+
+    public function logReportCreate(Report $report):void {
+        if (!$this->settings->has('sumologic.stats.collector')) {
+            return;
+        }
+        $this->collectLog(
+            type: 'report',
+            uuid: $report->uuid,
+            name: $report->profile->name,
+            uri: $report->target->getUri(),
+            targetType: get_class($report->target),
+            id: $report->target->getId(),
+            severity: $report->severity->value,
+            passes: count(array_filter($report->results, fn(AuditResponse $r) => $r->state->isSuccessful())),
+            failures: count(array_filter($report->results, fn(AuditResponse $r) => $r->state->isFailure())),
+            warnings: count(array_filter($report->results, fn(AuditResponse $r) => $r->state->hasWarning())),
+            errors: count(array_filter($report->results, fn(AuditResponse $r) => $r->state->hasError())),
+            irrelevant: count(array_filter($report->results, fn(AuditResponse $r) => $r->state->isIrrelevant())),
+            notices: count(array_filter($report->results, fn(AuditResponse $r) => $r->state->isNotice())),
+            policies: count($report->results),
+            start: $report->reportingPeriodStart->format('c'),
+            end: $report->reportingPeriodEnd->format('c'),
+            version: $this->settings->get('version'),
+            timing: $report->timing
+        );
+
+        foreach ($report->results as $response) {
+            $this->collectLog(
+                type: 'policy',
+                target: $report->target->getId(),
+                uuid: $report->uuid,
+                datetime: date('c', $response->timestamp),
+                name: $response->policy->name,
+                serverity: $response->getSeverity(),
+                status: $response->state->name,
+                timing: $response->timing
+            );
+        }
+    }
+
+    protected function collectLog(string $type, string|int|null|bool ...$fields):void {
+        $this->logs[$type][] = $fields;
+    }
+
+    public function __destruct()
+    {
+        if (empty($this->logs)) {
+            return;
+        }
+        $client = new Client();
+        foreach ($this->logs as $type => $logs) {
+            $payload = [];
+            foreach ($logs as $log) {
+                $payload[] = json_encode($log);
+            }
+            if (empty($payload)) {
+                continue;
+            }
+            
+            $client->post($this->settings->get('sumologic.stats.collector'), [
+                RequestOptions::BODY => implode(PHP_EOL, $payload),
+                RequestOptions::HEADERS => [
+                    'X-Sumo-Name' => $this->settings->get('name'),
+                    'X-Sumo-Category' => $type,
+                ]
+            ]);
+        }
+
+        
+    }
+}
